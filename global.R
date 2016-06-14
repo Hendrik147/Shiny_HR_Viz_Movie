ff9e6eb82d033b9e45b4607a19e10f160b5b7700
Hendrik Feddersen <Hendrik147@users.noreply.github.com>
2016-06-14 21:18:36 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,11 +1,12 @@
 # Variables that can be put on the x and y axes
 axis_vars <- c(
-"Flexi leave 2015" = "Flexi.leave.2015",
-"Certified sickleave 2015" = "Certified.sickleave.2015",
-"Uncertified sickleave 2015" = "Uncertified.sickleave.2015",
-"Days not recorded 2015" = "Days.not.recorded.2015",
-"Excess 2015" = "Excess.2015"
+"Flexi leave 2015" = "Flexileave2015",
+"Certified sickleave 2015" = "Certifiedsickleave2015",
+"Uncertified sickleave 2015" = "Uncertifiedsickleave2015",
+"Days not recorded 2015" = "Daysnotrecorded2015",
+"Excess 2015" = "Excess2015"
 )
+
 
 
 
