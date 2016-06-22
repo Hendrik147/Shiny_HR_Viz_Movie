a842c3eb71ba0a6118d7e03b6d126a6602306bf7
Hendrik Feddersen <Hendrik147@users.noreply.github.com>
2016-06-22 19:46:28 +0100

    Update prep work.R

--- prep work.R
+++ prep work.R
@@ -9,7 +9,12 @@ setwd("~/Developing Data Science Products/EMA")
 
 base <- read.csv("REPORT_RESULTa.csv", sep=";")
 base15 <- read.csv("REPORT_RESULT 2015a.csv", sep=";")
-all_flexitime <- merge(base, base15, by = "Personnel.Number..P.", all.x=TRUE)
+all_flexitimes <- merge(base, base15, by = "Personnel.Number..P.", all.x=TRUE)
+colnames(all_flexitimes) <- c("Number", "First", "Last", "Contract", "Grade", "Flexileave2015", "Certifiedsickleave2015", "Uncertifiedsickleave2015", "Daysnotrecorded2015", "Excess2015")
+all_flexitimes$Grade <- as.character(all_flexitimes$Grade)
+all_flexitimes$Contract <- as.character(all_flexitimes$Contract)
+all_flexitimes$First <- as.character(all_flexitimes$First)
+all_flexitimes$Last <- as.character(all_flexitimes$Last)
 
 #all_flexitime <- as.data.frame(all_flexitime)
 #dbWriteTable(all_flexitime, "all_flexitime", all_flexitime )
